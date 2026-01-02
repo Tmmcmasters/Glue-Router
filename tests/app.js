787fb701a834ac8ddec10fdd0a70897394a2febc
tmmcmasters <tmmcmasters@gmail.com>
2026-01-02 15:54:49 -0500

    Instead of alert is a text

--- tests/app.js
+++ tests/app.js
@@ -1,2 +1,4 @@
 console.log("app.js loaded and executed!");
-alert(`external script from ${location.pathname}`)
+
+document.querySelector("h1").style.color = "red";
+document.querySelector("main").insertAdjacentText("afterend", `External Script from ${location.pathname}`)
